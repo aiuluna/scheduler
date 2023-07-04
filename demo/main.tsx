b41212017b3d830a388a67f10666ee0f3e6bae77
aiuluna <604401072@qq.com>
2023-07-04 12:05:41 +0800

    feat: add demo

---
+++ demo/main.tsx
@@ -0,0 +1,17 @@
+const rootEle = document.getElementById('root');
+
+import './miniSchedule/index'
+
+// import './demo/MiniUpdate2State';
+// import './demo/MiniDiff';
+// import './demo/MiniUseState/index.ts';
+// import './demo/MiniSchedulePhase';
+// import system from './demo/MiniEventSystem'
+// const { addEvent, jsx } = system;
+
+// rootEle && ReactDOM.createRoot(rootEle).render(<App/>);
+// ReactDOM.render(<App/> , rootEle)
+
+
+// addEvent(rootEle, "click");
+// ReactDOM.createRoot(rootEle).render(jsx);
